dd449e5c458b12f686870876d51c3c43ffb7ebf1
ashutoshmohanty90-coder <ashutosh.mohanty90@gmail.com>
2026-05-20 15:09:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,57 +19,78 @@ function showTab(tabId){
 
 /* ENM DELETE */
 
-function generateENM(){
+function generateDelete(){
 
-    let site = document.getElementById("enmSite").value;
+    let site = document.getElementById("deleteSite").value;
 
     let commands = `
-cmedit get *XXXX* FmAlarmsupervision.*
+cmedit get *XXXX* FmAlarmsupervision.* ; Inventorysupervision.* ; pmFunction.*;cmNodeHeartbeatsupervision.*; cmFunction.* -t
 
 cmedit set NetworkElement=XXXX,CmNodeHeartbeatSupervision=1 active=false
 
+cmedit set NetworkElement=XXXX,InventorySupervision=1 active=false
+
+fmedit set XXXX FmAlarmSupervision alarmSupervisionState=false
+
+cmedit set NetworkElement=XXXX,PmFunction=1 pmEnabled=false
+
+cmedit action NetworkElement=XXXX,CmFunction=1 deleteNrmDataFromEnm
+
 cmedit delete NetworkElement=XXXX --ALL --force
+
+cmedit get NetworkElement=XXXX
+
+ap order -nv file:XXXX.zip
 
 ap status -n XXXX
 `;
 
     commands = commands.replaceAll("XXXX", site);
 
-    document.getElementById("enmOutput").innerText = commands;
+    document.getElementById("deleteOutput").innerText = commands;
 }
 
-/* CBRS */
+/* ENM ADD */
 
-function generateCBRS(){
+function generateAdd(){
 
-    let site = document.getElementById("cbrsSite").value;
+    let site = document.getElementById("addSite").value;
 
-    let commands = `
-cbrs remove XXXX
+    let ip = document.getElementById("ipAddress").value;
 
-cbrs status XXXX
-`;
-
-    commands = commands.replaceAll("XXXX", site);
-
-    document.getElementById("cbrsOutput").innerText = commands;
-}
-
-/* BACKUP */
-
-function generateBackup(){
-
-    let site = document.getElementById("backupSite").value;
+    let timezone = document.getElementById("timeZone").value;
 
     let commands = `
-backup create XXXX
+cmedit create NetworkElement=YYYY networkElementId=YYYY, neType=RadioNode, ossPrefix="SubNetwork=ONRM_ROOT_MO,SubNetwork=Bloomfield,MeContext=YYYY" -ns=OSS_NE_DEF -v=2.0.0
 
-backup verify XXXX
+cmedit create NetworkElement=YYYY,ComConnectivityInformation=1 ComConnectivityInformationId=1, transportProtocol=TLS, ipAddress="IPADDRESS", port=6513, snmpSecurityName=defaultsnmpuser, snmpVersion=SNMP_V3, snmpSecurityLevel=NO_AUTH_NO_PRIV -ns=COM_MED -v=1.1.0
+
+secadm credentials create -sn prbs -sp "@WtIwQ-Bo2m0" -n YYYY
+
+cmedit set NetworkElement=YYYY timeZone=TIMEZONE
+
+cmedit set NetworkElement=YYYY managementState=NORMAL
+
+alarm enable YYYY
+
+cmedit set NetworkElement=YYYY,SecurityFunction=1,NetworkElementSecurity=1 algorithmAndKeySize=RSA_2048, enrollmentMode=CMPv2_VC
+
+cmedit set NetworkElement=YYYY,CmNodeHeartbeatSupervision=1 active=true
+
+cmedit set NetworkElement=YYYY,InventorySupervision=1 active=true
+
+fmedit set YYYY FmAlarmSupervision alarmSupervisionState=true
+
+cmedit set NetworkElement=YYYY,PmFunction=1 pmEnabled=true
 `;
 
-    commands = commands.replaceAll("XXXX", site);
+    commands = commands.replaceAll("YYYY", site);
 
-    document.getElementById("backupOutput").innerText = commands;
+    commands = commands.replaceAll("IPADDRESS", ip);
+
+    commands = commands.replaceAll("TIMEZONE", timezone);
+
+    document.getElementById("addOutput").innerText = commands;
 }
 
 /* COPY */
